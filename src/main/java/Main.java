b1016c1f47998d848dc8c9f04b14b5041bc1ad3a
Suman Kashyap <sumankashyap@microsoft.com>
2024-02-17 21:11:37 -0800

    Add logic for InfoReply

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -239,13 +239,14 @@ public class Main {
     public void outputRespResponse(PrintWriter output) {
       String replicationString = "# Replication";
       System.out.println(replicationString);
-      output.print(String.format("$%d\r\n%s\r\n", replicationString.length(), replicationString));
+      output.printf("$%d\r\n%s\r\n", "role: master".length(), "role: master");
+      output.printf("$%d\r\n%s\r\n", replicationString.length(), replicationString);
       try {
         Field[] fields = this.getClass().getDeclaredFields();
         for (Field f : fields) {
           String line = String.format("%s : %s", f.getName(), f.get(this).toString());
           System.out.println(line);
-          output.print(String.format("$%d\r\n%s\r\n", line.length(), line));
+          output.printf("$%d\r\n%s\r\n", line.length(), line);
         }
       }
       catch (IllegalAccessException ex) {
