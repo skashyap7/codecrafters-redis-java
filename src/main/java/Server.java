e52730f734328cc03c2ae99d2b1f285b09497dc1
Suman Kashyap <sumankashyap@microsoft.com>
2024-03-10 18:53:28 -0700

    Send an empty RDB file as a response to PSYCN

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -239,7 +239,8 @@ public class Server {
             String response = "+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0";
             output.printf("%s\r\n",response);
             // Send the empty RDB file
-            output.printf("$%d\r\n%s", 0, "\r\n");
+            String emptyData = "\r\n";
+            output.printf("$%d\r\n%s", emptyData.length(), emptyData);
         }
         private void executeEcho(PrintWriter output) {
             String outputStr = String.join(" ",arguments);
